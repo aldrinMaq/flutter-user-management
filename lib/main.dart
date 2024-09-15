e96261b2c3d0d577de8811815b574cf0ba32c078
Aldrin <aamaquiling@nemsu.edu.ph>
2024-09-15 19:28:59 +0800

    Revert "fixed display with dismissible"
    This reverts commit dca86a6616ea7d18f5c74bdc18b55b6e386bf674.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'officers.dart'; // Ensure OfficerPage is imported
+
+import 'officers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,12 +28,11 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
 
-  // Update the sections here
   static const List<Widget> _widgetOptions = <Widget>[
-    Text('Newsfeed', style: TextStyle(fontSize: 24)),
-    Text('QR Generator', style: TextStyle(fontSize: 24)),
-    Text('Map', style: TextStyle(fontSize: 24)),
-    OfficerPage(), // Display OfficerPage when "Users" is clicked
+    OfficerPage(),
+    Text('Events'),
+    Text('Map'),
+    Text('Profile'),
   ];
 
   void _onItemTapped(int index) {
@@ -44,48 +44,32 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Use SafeArea to avoid overlapping with system UI
-      body: SafeArea(
-        child: SingleChildScrollView(
-          // Allow scrolling if content overflows
-          child: Container(
-            height: MediaQuery.of(context).size.height -
-                kBottomNavigationBarHeight, // Subtract nav bar height
-            child: Center(
-              child: _widgetOptions.elementAt(_selectedIndex),
-            ),
-          ),
-        ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      // BottomAppBar with custom background color
-      bottomNavigationBar: BottomAppBar(
-        color: const Color.fromARGB(255, 20, 17, 201), // Custom color
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.transparent, // Transparent over BottomAppBar
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.feed),
-              label: 'Newsfeed',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.qr_code),
-              label: 'QR Generator',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.map),
-              label: 'Map',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Users',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.white70,
-          onTap: _onItemTapped,
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: 'Events',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
       ),
     );
   }
--- lib/officers.dart
+++ lib/officers.dart
@@ -9,127 +9,20 @@ class OfficerPage extends StatefulWidget {
 
 class _OfficerPageState extends State<OfficerPage> {
   List<Map<String, dynamic>> users = [
-    {'name': 'Aldrin A. Maquiling', 'role': 'Officer', 'position': 'President'},
-    {
-      'name': 'Krisia Helena May C. Octal',
-      'role': 'Officer',
-      'position': 'Vice President'
-    },
-    {'name': 'Michell Lubrio', 'role': 'Officer', 'position': 'Secretary'},
+    {'name': 'Aldrin A. Maquiling', 'role': 'Officer'},
+    {'name': 'Krisia Helena May C. Octal', 'role': 'Officer'},
+    {'name': 'Michell Lubrio', 'role': 'Officer'},
   ];
-
-  List<Map<String, dynamic>> filteredUsers = [];
-  Map<String, dynamic>? _recentlyRemovedUser;
-  int? _recentlyRemovedUserIndex;
-
-  final TextEditingController searchController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    filteredUsers = users; // Initially, all users are shown
-  }
-
-  void _filterUsers(String searchQuery) {
-    setState(() {
-      filteredUsers = users
-          .where((user) =>
-              user['name'].toLowerCase().contains(searchQuery.toLowerCase()))
-          .toList();
-    });
-  }
-
-  void _addNewUser(String name, String role, String position) {
-    setState(() {
-      users.add({'name': name, 'role': role, 'position': position});
-      filteredUsers = users; // Update the filtered list
-    });
-  }
-
-  void _showAddUserDialog() {
-    final TextEditingController nameController = TextEditingController();
-    final TextEditingController positionController = TextEditingController();
-    String selectedRole = 'Officer'; // Default role
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Add New User'),
-          content: StatefulBuilder(
-            builder: (BuildContext context, StateSetter setState) {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextField(
-                    controller: nameController,
-                    decoration: const InputDecoration(labelText: 'Name'),
-                  ),
-                  const SizedBox(height: 20),
-                  TextField(
-                    controller: positionController,
-                    decoration: const InputDecoration(labelText: 'Position'),
-                  ),
-                  const SizedBox(height: 20),
-                  DropdownButton<String>(
-                    value: selectedRole,
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        selectedRole = newValue!;
-                      });
-                    },
-                    items: <String>['Officer', 'Admin']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
-                ],
-              );
-            },
-          ),
-          actions: [
-            TextButton(
-              child: const Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            ElevatedButton(
-              child: const Text('Add'),
-              onPressed: () {
-                if (nameController.text.isNotEmpty &&
-                    positionController.text.isNotEmpty) {
-                  _addNewUser(nameController.text, selectedRole,
-                      positionController.text);
-                  Navigator.of(context).pop();
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Please enter both name and position.'),
-                    ),
-                  );
-                }
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'User Management',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: const Color.fromARGB(255, 20, 17, 201),
-      ),
+          title: const Text(
+            'User Management',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: const Color.fromARGB(255, 20, 17, 201)),
       body: Column(
         children: [
           Padding(
@@ -139,137 +32,90 @@ class _OfficerPageState extends State<OfficerPage> {
             ),
             child: Row(
               children: [
-                Expanded(
+                const Expanded(
                   child: TextField(
-                    controller: searchController,
-                    decoration: const InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintText: 'Search name',
-                      hintStyle: TextStyle(color: Colors.grey),
-                    ),
-                    onChanged: _filterUsers, // Search functionality
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Input name / user id',
+                        hintStyle: TextStyle(color: Colors.grey)),
                   ),
                 ),
-                const SizedBox(width: 20),
-                IconButton(
-                  icon: const Icon(Icons.add),
-                  color: Colors.blue,
-                  iconSize: 30,
-                  onPressed: _showAddUserDialog, // Add new user dialog
+                const SizedBox(width: 30),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 18, 15, 225),
+                      foregroundColor: Colors.white,
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                      )),
+                  child: const Text('Add user'),
                 ),
               ],
             ),
           ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: DataTable(
-                columns: const [
-                  DataColumn(
+          SizedBox(
+            width: double.infinity,
+            child: DataTable(
+              columns: const [
+                DataColumn(
                     label: Text(
-                      'Name',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  DataColumn(
+                  'Name',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                )),
+                DataColumn(
                     label: Text(
-                      'Role',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  DataColumn(
+                  'Role',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                )),
+                DataColumn(
                     label: Text(
-                      'Position',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Action',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ],
-                rows: filteredUsers.map((user) {
-                  return DataRow(
-                    cells: [
-                      DataCell(Text(user['name'])),
-                      DataCell(Text(user['role'])),
-                      DataCell(Text(user['position'])),
-                      DataCell(
-                        Dismissible(
-                          key:
-                              UniqueKey(), // Ensures each Dismissible has a unique key
-                          onDismissed: (direction) {
+                  'Action',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                )),
+              ],
+              rows: users.map((user) {
+                return DataRow(
+                  cells: [
+                    DataCell(Text(user['name'])),
+                    DataCell(Text(user['role'])),
+                    DataCell(Row(
+                      children: [
+                        PopupMenuButton<String>(
+                          icon: const Icon(
+                            Icons.settings,
+                            color: Colors.grey,
+                          ),
+                          onSelected: (String? newValue) {
                             setState(() {
-                              _recentlyRemovedUser = user;
-                              _recentlyRemovedUserIndex = users.indexOf(user);
-                              users.remove(
-                                  user); // Remove the user from the list
-                              filteredUsers = users;
+                              user['role'] = newValue;
                             });
-
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text('${user['name']} dismissed'),
-                                action: SnackBarAction(
-                                  label: 'Undo',
-                                  onPressed: () {
-                                    // Undo the dismissal by adding the user back to the list
-                                    setState(() {
-                                      if (_recentlyRemovedUser != null &&
-                                          _recentlyRemovedUserIndex != null) {
-                                        users.insert(_recentlyRemovedUserIndex!,
-                                            _recentlyRemovedUser!);
-                                        filteredUsers = users;
-                                        _recentlyRemovedUser = null;
-                                        _recentlyRemovedUserIndex = null;
-                                      }
-                                    });
-                                  },
-                                ),
-                                duration: const Duration(
-                                    seconds: 5), // Visible for 5 seconds
-                              ),
-                            );
                           },
-                          background: Container(
-                            color: Colors.red,
-                            alignment: Alignment.centerRight,
-                            padding: const EdgeInsets.symmetric(horizontal: 20),
-                            child: const Icon(
-                              Icons.delete,
-                              color: Colors.white,
+                          itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                            const PopupMenuItem<String>(
+                              value: 'Admin',
+                              child: Text('Admin'),
                             ),
-                          ),
-                          child: PopupMenuButton<String>(
-                            icon: const Icon(
-                              Icons.settings,
-                              color: Colors.grey,
+                            const PopupMenuItem<String>(
+                              value: 'Officer',
+                              child: Text('Officer'),
                             ),
-                            onSelected: (String? newValue) {
-                              setState(() {
-                                user['role'] = newValue;
-                              });
-                            },
-                            itemBuilder: (BuildContext context) =>
-                                <PopupMenuEntry<String>>[
-                              const PopupMenuItem<String>(
-                                value: 'Admin',
-                                child: Text('Admin'),
-                              ),
-                              const PopupMenuItem<String>(
-                                value: 'Officer',
-                                child: Text('Officer'),
-                              ),
-                            ],
-                          ),
+                            // const PopupMenuItem<String>(
+                            //   value: 'User',
+                            //   child: Text('User'),
+                            // ),
+                          ],
                         ),
-                      ),
-                    ],
-                  );
-                }).toList(),
-              ),
+                        const Icon(
+                          Icons.close,
+                          color: Colors.red,
+                        ),
+                      ],
+                    )),
+                  ],
+                );
+              }).toList(),
             ),
           ),
         ],
